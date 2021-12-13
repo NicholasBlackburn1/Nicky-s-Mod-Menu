3297bba7197130df455f18d1063e27d0a3337ce1
nickb <nickblackburn02@gmail.com>
2021-12-13 13:26:50 -0800

    got basic mod to build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using MelonLoader;
 
-[assembly: AssemblyTitle(TestMod.BuildInfo.Description)]
-[assembly: AssemblyDescription(TestMod.BuildInfo.Description)]
-[assembly: AssemblyCompany(TestMod.BuildInfo.Company)]
-[assembly: AssemblyProduct(TestMod.BuildInfo.Name)]
-[assembly: AssemblyCopyright("Created by " + TestMod.BuildInfo.Author)]
-[assembly: AssemblyTrademark(TestMod.BuildInfo.Company)]
-[assembly: AssemblyVersion(TestMod.BuildInfo.Version)]
-[assembly: AssemblyFileVersion(TestMod.BuildInfo.Version)]
-[assembly: MelonInfo(typeof(TestMod.TestMod), TestMod.BuildInfo.Name, TestMod.BuildInfo.Version, TestMod.BuildInfo.Author, TestMod.BuildInfo.DownloadLink)]
+[assembly: AssemblyTitle(NickysModMenu.BuildInfo.Description)]
+[assembly: AssemblyDescription(NickysModMenu.BuildInfo.Description)]
+[assembly: AssemblyCompany(NickysModMenu.BuildInfo.Company)]
+[assembly: AssemblyProduct(NickysModMenu.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + NickysModMenu.BuildInfo.Author)]
+[assembly: AssemblyTrademark(NickysModMenu.BuildInfo.Company)]
+[assembly: AssemblyVersion(NickysModMenu.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(NickysModMenu.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(NickysModMenu.Main), NickysModMenu.BuildInfo.Name, NickysModMenu.BuildInfo.Version, NickysModMenu.BuildInfo.Author, NickysModMenu.BuildInfo.DownloadLink)]
 [assembly: MelonColor()]
 
 // Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
